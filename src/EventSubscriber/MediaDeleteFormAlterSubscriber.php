580e64a625ba49235701a89408d2ef632767446e
Dieter Holvoet <dieter.holvoet@gmail.com>
2019-01-04 16:07:23 +0100

    Show confirmation before deleting fields referencing a media entity

---
+++ src/EventSubscriber/MediaDeleteFormAlterSubscriber.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\wmmedia\EventSubscriber;
+
+use Drupal\Core\Entity\ContentEntityDeleteForm;
+use Drupal\Core\Entity\FieldableEntityInterface;
+use Drupal\Core\Field\FieldItemInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\hook_event_dispatcher\Event\Form\FormIdAlterEvent;
+use Drupal\media\MediaInterface;
+use Drupal\wmmedia\Service\MediaReferenceDiscovery;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class MediaDeleteFormAlterSubscriber implements EventSubscriberInterface
+{
+    use StringTranslationTrait;
+
+    /** @var MediaReferenceDiscovery */
+    protected $referenceDiscovery;
+
+    public function __construct(
+        MediaReferenceDiscovery $mediaReferenceDiscovery
+    ) {
+        $this->referenceDiscovery = $mediaReferenceDiscovery;
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            'hook_event_dispatcher.form_media_image_delete_form.alter' => 'onAlter',
+        ];
+    }
+
+    public function onAlter(FormIdAlterEvent $event)
+    {
+        $form = &$event->getForm();
+        /** @var ContentEntityDeleteForm $formObject */
+        $formObject = $event->getFormState()->getFormObject();
+        /** @var MediaInterface $entity */
+        $entity = $formObject->getEntity();
+        $usages = $this->referenceDiscovery->getUsages($entity);
+
+        $markup = sprintf(
+            '<p>%s</p>',
+            $this->formatPlural(count($usages), 'Caution, this image is being referenced by 1 entity:', 'Caution, this image is being referenced by @count entities.')
+        );
+
+        $markup .= '<ul>';
+        $markup .= implode(PHP_EOL, array_map(function (FieldableEntityInterface $entity) {
+            $label = $entity->label();
+
+            if (empty($entity->label())) {
+                $label = sprintf('<i>%s</i>', $this->t('Entity of type :bundle', [':bundle' => $entity->bundle()]));
+            }
+
+            if ($entity->hasLinkTemplate('edit-form')) {
+                return sprintf('<li><a href="%s" target="_blank" rel="noopener noreferrer">%s</a></li>', $entity->toUrl('edit-form')->toString(), $label);
+            }
+
+            return sprintf('<li>%s</li>', $label);
+        }, $usages));
+        $markup .= '</ul>';
+
+        if ($this->hasRequiredUsages($usages)) {
+            $form['actions']['submit']['#disabled'] = true;
+            $markup .= sprintf(
+                '<p>%s</p>',
+                $this->formatPlural(count($usages), 'This field is required. Please delete it before deleting the image.', 'One of these fields is required. Please remove all required fields before deleting the image.')
+            );
+        } else {
+            $markup .= sprintf(
+                '<p>%s</p><p><b>%s</b></p>',
+                $this->t('When deleting this image, all referencing fields will be deleted.'),
+                $this->t('Are you sure you want to proceed? This action cannot be undone.')
+            );
+        }
+
+        $form['description']['#markup'] = $markup;
+    }
+
+    /** @param $usages FieldableEntityInterface[] */
+    protected function hasRequiredUsages(array $usages)
+    {
+        foreach ($usages as $fieldName => $usage) {
+            /** @var FieldItemInterface $field */
+            foreach ($usage->get($fieldName) as $field) {
+                $fieldDefinition = $field->getFieldDefinition();
+
+                if ($fieldDefinition->isRequired()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
